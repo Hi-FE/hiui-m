1ae6fa089118e66a44b1ee944501cb164bba790c
NightCatSama <642163903@qq.com>
2017-04-12 17:09:53 +0800

    修改config

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'hiui-m/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: 'hiui-m/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
